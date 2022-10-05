20f5c383b696e8325da354d535947a2d03823278
Mohand Kessi <mohandkessi808@gmail.com>
2022-10-04 22:37:30 -0400

    La calculatrice est désormais fonctionnelle seulement 2 chiffres et pas de décimales prises en compte

--- app/src/main/java/com/example/calculatrice/MainActivity.kt
+++ app/src/main/java/com/example/calculatrice/MainActivity.kt
@@ -11,12 +11,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
     }
+    var nb1 : Double? = null
+
     fun chiffre(view: View) {
         var edit = findViewById(R.id.letexte) as EditText
         var letext = edit.text.toString() + (view as Button).text
         // Do something in response to button click
         edit.setText(letext)
+    }
+    var operation = ""
+    var result = 0.0
+    fun operation(view : View){
+
+        var edit = findViewById(R.id.letexte) as EditText
+        if(nb1==null){
+            nb1 = edit.text.toString().toDouble()
+            operation = (view as Button).text.toString()
+            edit.setText("")
+
+        }else{
+            if(operation=="+"){result = nb1!! + edit.text.toString().toDouble()}
+            else if(operation=="-"){result = nb1!! - edit.text.toString().toDouble()}
+            else if(operation=="*"){result = nb1!! * edit.text.toString().toDouble()}
+            else if(operation=="/"){result = nb1!! / edit.text.toString().toDouble()}
+            edit.setText(result.toString())
+
+        }
+
+
+
+
     }
 
 }

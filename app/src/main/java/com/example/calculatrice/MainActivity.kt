d4af6d280ed6f9d1e2a037385a81224fece53ab4
Mohand Kessi <mohandkessi808@gmail.com>
2022-10-04 22:00:47 -0400

    Ajout des boutons fonctionnels et de la methode calcul

--- app/src/main/java/com/example/calculatrice/MainActivity.kt
+++ app/src/main/java/com/example/calculatrice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.calculatrice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    fun chiffre(view: View) {
+        var edit = findViewById(R.id.letexte) as EditText
+        var letext = edit.text.toString() + (view as Button).text
+        // Do something in response to button click
+        edit.setText(letext)
+    }
+
 }
